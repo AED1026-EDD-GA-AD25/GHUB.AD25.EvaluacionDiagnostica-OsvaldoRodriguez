76090e937d31ba709aa1f2725edda2aad9846c0a
Osvaldo Rodriguez Armenta <158524993+OsvaldoRA@users.noreply.github.com>
2025-08-25 17:59:12 +0000

    mostrar

--- miPrincipal/InventarioLibros.java
+++ miPrincipal/InventarioLibros.java
@@ -1,10 +1,28 @@
 package miPrincipal;
 
 public class InventarioLibros{
+    Libro listaLibro[];//creando arreglo de libro
+    int contador;
     /*Constructores*/
+    public InventarioLibros(){
+        listaLibro = new Libro[2];
+        contador = 0;
+    }
     /*Metodos */
     /* agrega libros a la lista */
     public void agregar(Libro libro){
+        if(contador<listaLibro.length){
+        listaLibro[contador] = libro;
+        contador++;    
+        }else{
+            Libro listaTemp[] = new Libro[listaLibro.length*2];
+            for(int i=0;i<listaLibro.length;i++){
+                listaTemp[i]=listaLibro[i];
+            }
+            listaLibro = listaTemp;
+            listaLibro[contador] = libro;
+            contador++;
+        }
         
     }
     /*busca en la lista un libro por autor, si no retorna null  */
@@ -14,6 +32,9 @@ public class InventarioLibros{
     }
     /*muestra por pantalla toda la lista de libros */
     public void mostrar(){
+        for(int i=0;i<contador;i++){
+            System.out.println(listaLibro[i]);
+        }
 
     }
     
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,13 +1,54 @@
 package miPrincipal;
+import java.util.Scanner;
 
 public class Principal {
-    public String getGreeting() {
-        return "Hola mundo!";
-    }
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println(new Principal().getGreeting());
+        int opc =0;
+        String autor,titulo = null;
+        int anio = 0;
+        InventarioLibros inventarioLibros = new InventarioLibros();
+        do {
+            menu();
+            opc = sc.nextInt();
+            sc.nextLine();//consume el entero
+            switch (opc) {
+                case 0:
+                System.out.println("Chao");
+                break;
+                case 1:
+                System.out.println("Proporcion nombre de autor: ");
+                autor=sc.nextLine();
+                System.out.println("Proporciona titulo del libro: ");
+                titulo=sc.nextLine();
+                System.out.println("Proporciona año de publicacion: ");
+                anio=sc.nextInt();
+                break;
+                case 2:
+                System.out.println("Proporcion autor del libro: ");
+                autor=sc.nextLine();
+                Libro libro = new Libro(titulo, autor, anio);
+                inventarioLibros.agregar(libro);
+                break;
+                case 3:
+                inventarioLibros.mostrar(); 
+                break;   
+                default:
+                    break;
+            }
+        } while (opc !=0);
 
+
+    }
+    public static void menu(){
+        System.out.println("Menu Principal");
+        System.out.println("1. Agregar");
+        System.out.println("2. Buscar");
+        System.out.println("3. Mostrar todo");
+        System.out.println();
+        System.out.println("0. Salir");
+        System.out.println("Selecciona opcion: ");
 
     }
 }
