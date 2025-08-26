35840ef9ee11ca19fff3c47fbef95213df765a29
Osvaldo Rodriguez Armenta <158524993+OsvaldoRA@users.noreply.github.com>
2025-08-26 17:24:34 +0000

    Arreglo

--- miPrincipal/DemostrarArreglos.java
+++ miPrincipal/DemostrarArreglos.java
@@ -1,42 +1,50 @@
 package miPrincipal;
 public class DemostrarArreglos{
     public static void main(String[] args) {
-        System.out.println("Uso de Arreglos ");
-        int numero1[];
-        numero1 = new int[3];
-        short numero2[];
-        numero2 = new short[5];
-        double numero3[] = new double[10];
+        System.out.println("Uso de Arreglos");
+        int numeros1[];
+        numeros1 = new int[3];
 
-        long numero4[] = new long[3];
-        Libro arregloLibros[]/*= new Libro[5]; */;
-        arregloLibros = new Libro[5];
+        short []numeros2;
+        numeros2 = new short[5];
 
-        numero1[2]=10;
-        System.out.println("Arreglo de entero");
-        for(int i=0;i<3;i++){
-            System.out.println(numero1[i]);
+        double[] numeros3 = new double[10];
+        long numeros4[] = new long[3];
 
-        }
-        System.out.println("Arreglo de short");
-        for(int i=0;i<numero2.length;i++){
-            System.out.println(numero2[i]);
+       // Libro arregloLibros[] =  new Libro[5];
+       Libro arregloLibros[] ;
+       arregloLibros = new Libro[5];
 
-        }
-        System.out.println("Arreglo de Libros");
-        arregloLibros[0]= new Libro("El invencible verano", "Marta", 2003);
-        arregloLibros[1]= new Libro();
-        arregloLibros[2]= new Libro();
-        arregloLibros[3]= new Libro();
-        arregloLibros[4]= new Libro();
-        for(int i=0;i<arregloLibros.length;i++){
-            System.out.println(arregloLibros[i]);
-           /// System.out.println(arregloLibros[i].gettitulo());
+       numeros1[2]=10;
+       System.out.println("Arreglo de Enteros:");
+       for(int i=0;i<3;i++){
+        System.out.println(numeros1[i]);
+       }
+       System.out.println("Arreglo de Short:");
+       for(int i=0;i<numeros2.length;i++){
+        System.out.println(numeros2[i]);
+       }
 
-        }
+       System.out.println("Arreglo de Libros:");
+       arregloLibros[0]= new Libro("El invencible verano","Ana Cristina",2011);
+       arregloLibros[1] = new Libro();
+       arregloLibros[1].setautor("Gabriel Garcia Marquez");
+       arregloLibros[1].settitulo("Cien años de soledad");
+       arregloLibros[1].setanio(1986);
+       arregloLibros[2] = new Libro();
+       arregloLibros[3] = new Libro();
+       arregloLibros[4] = new Libro();
+
+       for(int i=0;i<arregloLibros.length;i++){
+        System.out.println(arregloLibros[i]);
+        System.out.println(arregloLibros[i].gettitulo());
+
+       }
+
+
+
+
+        
     }
-
-    
-
     
 }
